36d3d7a8b783c7322a36e5d7bf47331230170050
hermer29 <kliman.chan@gmail.com>
2026-05-20 13:45:00 +0300

    Change Jenny Generation Path

--- Assets/_App/CodeBase/Generated/Game/GameComponentsLookup.cs
+++ Assets/_App/CodeBase/Generated/Game/GameComponentsLookup.cs
@@ -8,15 +8,15 @@
 //------------------------------------------------------------------------------
 public static class GameComponentsLookup {
 
+    public const int Input = 0;
 
-
-    public const int TotalComponents = 0;
+    public const int TotalComponents = 1;
 
     public static readonly string[] componentNames = {
-
+        "Input"
     };
 
     public static readonly System.Type[] componentTypes = {
-
+        typeof(GameplayLayer.Input.Components.InputComponent)
     };
 }
